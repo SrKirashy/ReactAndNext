90853a0ade414940afa6ce23451b9d8e0b419731
Bruno Santos Silva <jkbrunowk@hotmail.com>
2025-08-31 12:20:53 -0300

    Page Configurado. Components, Types e Data, criados.

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <>
+    <div className="w-screen h-screen text-center flex justify-center items-center flex-col">
+      <h1 className="w-auto h-scren text-6xl font-mono">Olá, Mundo...</h1>
+      <p className="w-auto h-scren text-2xl font-mono">Aqui até o fim, é só o começo. Sejam bem vindos a <span className="font-bold text-violet-400">NextLife</span>.</p>
+    </div>
+      
+    </>
+  )
+}
+
+export default App;
